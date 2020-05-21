eb49d15e21149bfad26591a0ec793bb9fccd9983
alexanderajju <alexanderajju@gmail.com>
2020-05-21 21:02:29 +0530

    Updates

---
+++ precache-manifest.25c5de7231ebe06862ddd242ae9b0e10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "294b049c579c7e8e25acfb3bde891da5",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "9c6bec0f0b560cf86953",
+    "url": "/todoapp/static/css/main.a92980bf.chunk.css"
+  },
+  {
+    "revision": "8fcf3b09d7c572624dfb",
+    "url": "/todoapp/static/js/2.1d602a41.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todoapp/static/js/2.1d602a41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c6bec0f0b560cf86953",
+    "url": "/todoapp/static/js/main.4cff28bb.chunk.js"
+  },
+  {
+    "revision": "9b2f915f09a204b07370",
+    "url": "/todoapp/static/js/runtime-main.d779cf41.js"
+  }
+]);
